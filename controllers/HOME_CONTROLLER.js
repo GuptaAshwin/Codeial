afb4291eeee2043ea461ed5db1a31c66e4a180c3
Ashwin Dinesh Gupta <ashwinguptaak@gmail.com>
2022-01-15 19:47:49 +0530

    saving to github

--- controllers/HOME_CONTROLLER.js
+++ controllers/HOME_CONTROLLER.js
@@ -1,3 +1,5 @@
 module.exports.home= function(req,res){
-    return res.end('<h1>COntroller is up for running!!</h1>');
+    return res.render('home',{
+        title:"Home Sweet Home"
+        });
 }
