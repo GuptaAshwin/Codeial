a1cbc2c26a473ee196b226c4b2635d36031b3f46
Ashwin Dinesh Gupta <ashwinguptaak@gmail.com>
2022-01-15 13:51:31 +0530

    adding users profile router and controller

--- route/index.js
+++ route/index.js
@@ -2,5 +2,6 @@ const express =require('express');
 const router =express.Router();
 const homeController=require('../controllers/Home_Controller');
 console.log("router loaded");
-router.get('/',homeController.home)
+router.get('/',homeController.home);
+router.use('/users',require('./users'));
 module.exports=router;
