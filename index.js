e334a07476fecff73320e32c8f6e66b45a55bbab
Ashwin Dinesh Gupta <ashwinguptaak@gmail.com>
2022-01-15 13:01:11 +0530

    controller and router added

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const app= express();
 const port=8000;
-
+app.use('/',require('./route'));
 app.listen(port,function(err){
     if(err){
         console.log(`Error in running our server : ${err}`);
