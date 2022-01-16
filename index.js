7f08e56a0363d653f26d96b061ce689f534bc5b8
Ashwin Dinesh Gupta <ashwinguptaak@gmail.com>
2022-01-17 00:25:42 +0530

    saving to github

--- index.js
+++ index.js
@@ -1,12 +1,21 @@
 const express = require('express');
 const app= express();
 const port=8000;
+
+const expressejslayout= require('express-ejs-layouts');
+
+app.use(express.static('./assets/'));
+
+app.use(expressejslayout);
+// use express router
 app.use('/',require('./route'));
+
+// setup a view engine 
 app.set('view engine','ejs');
 app.set('views','./views');
 app.listen(port,function(err){
     if(err){
-        console.log(`Error in running our server : ${err}`);
+        console.log(`Error in running  server : ${err}`);
     }
-    console.log(`Server is Running algorithm!!! :${port}`);
+    console.log(`Server is Runningalgorithm!!! :${port}`);
 });
