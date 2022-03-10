d218fc32535349bbe548f36081d3fe774de85df3
Ashwin Dinesh Gupta <ashwinguptaak@gmail.com>
2022-03-10 21:08:45 +0530

    Added Comment and Populated it

--- controllers/HOME_CONTROLLER.js
+++ controllers/HOME_CONTROLLER.js
@@ -2,7 +2,15 @@ const Post = require('../models/post');
 
 module.exports.home = function(req, res){
    
-Post.find({}).populate('user').exec(function(req,posts){
+Post.find({})
+.populate('user')
+.populate({
+    path: 'comments',
+    populate:{
+        path:'user'
+    }
+})
+.exec(function(req,posts){
     return res.render('home', {
         title: " Codieal | Home",
         posts: posts
