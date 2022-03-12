2e86a3bcbb5e21b56dd17f4d20cc9b08311d3aa4
Ashwin Dinesh Gupta <ashwinguptaak@gmail.com>
2022-03-12 12:51:49 +0530

    Deleting and Updating Objects in Database + Distributing Views

--- controllers/HOME_CONTROLLER.js
+++ controllers/HOME_CONTROLLER.js
@@ -1,22 +1,26 @@
 const Post = require('../models/post');
+const User = require('../models/user');
 
-module.exports.home = function(req, res){
-   
-Post.find({})
-.populate('user')
-.populate({
-    path: 'comments',
-    populate:{
-        path:'user'
-    }
-})
-.exec(function(req,posts){
-    return res.render('home', {
-        title: " Codieal | Home",
-        posts: posts
-    });
-});  
-};
+module.exports.home = function (req, res) {                      //modules.exports.Actionname=callback function ;
+    
+    // populate the user of each post 
+    Post.find({})
+    .populate('user')
+    .populate({
+        path: 'comments',
+        populate:{
+            path:'user'
+        }
+    }) 
+    .exec(function(err, posts) {
 
-
-// module.exports.actionName = function(req, res){}
+        User.find({}, function(err, users){
+            return res.render('home', {
+                title: " Codiel | Home ",
+                posts: posts,
+                all_users: users
+            });
+    
+        });
+    })
+}
--- controllers/users_Controller.js
+++ controllers/users_Controller.js
@@ -1,16 +1,31 @@
+const { send } = require('express/lib/response');
 const User = require('../models/user');
 
 
-module.exports.profile = function(req, res){
-    return res.render('user_profile', {
-        title: 'User Profile'
-    })
+module.exports.profile = function (req, res) {
+
+    User.findById(req.params.id, function (err, user) {
+        return res.render('user_profile', {
+            title: 'User Profile',
+            profile_user: user
+        });
+    });
+
 }
 
+module.exports.update = function(req, res){
+    if(req.user.id == req.params.id){
+        User.findByIdAndUpdate(req.params.id , req.body, function(err, user){
+            return res.redirect('back');
+        });
+    }else{
+        return res.status(401).send('Unauthorized');
+    };
+};
 
 // render the sign up page
-module.exports.signUp = function(req, res){
-    if (req.isAuthenticated()){
+module.exports.signUp = function (req, res) {
+    if (req.isAuthenticated()) {
         return res.redirect('/users/profile');
     }
 
@@ -22,9 +37,9 @@ module.exports.signUp = function(req, res){
 
 
 // render the sign in page
-module.exports.signIn = function(req, res){
+module.exports.signIn = function (req, res) {
 
-    if (req.isAuthenticated()){
+    if (req.isAuthenticated()) {
         return res.redirect('/users/profile');
     }
     return res.render('user_sign_in', {
@@ -33,21 +48,21 @@ module.exports.signIn = function(req, res){
 }
 
 // get the sign up data
-module.exports.create = function(req, res){
-    if (req.body.password != req.body.confirm_password){
+module.exports.create = function (req, res) {
+    if (req.body.password != req.body.confirm_password) {
         return res.redirect('back');
     }
 
-    User.findOne({email: req.body.email}, function(err, user){
-        if(err){console.log('error in finding user in signing up'); return}
+    User.findOne({ email: req.body.email }, function (err, user) {
+        if (err) { console.log('error in finding user in signing up'); return }
 
-        if (!user){
-            User.create(req.body, function(err, user){
-                if(err){console.log('error in creating user while signing up'); return}
+        if (!user) {
+            User.create(req.body, function (err, user) {
+                if (err) { console.log('error in creating user while signing up'); return }
 
                 return res.redirect('/users/sign-in');
             })
-        }else{
+        } else {
             return res.redirect('back');
         }
 
@@ -56,12 +71,12 @@ module.exports.create = function(req, res){
 
 
 // sign in and create a session for the user
-module.exports.createSession = function(req, res){
+module.exports.createSession = function (req, res) {
     return res.redirect('/');
 }
 
-module.exports.destroySession = function(req, res){
-  
+module.exports.destroySession = function (req, res) {
+
     req.logout();
 
     return res.redirect('/');
